e9dc5c8e4a71c2b5de6799fb1e291048f4f3be1a
Luka Bonheure <Luka.Bonheure@ulb.be>
2023-05-23 19:46:57 +0200

    delete dossier tree

--- src/test_tree.py
+++ src/test_tree.py
@@ -27,32 +27,3 @@ if __name__=='__main__':
     initialState = State(shapes, s)
     action = tree.search(initialState)
     print(action)
-    
-
-
-
-"""
-if __name__=='__main__':
-    def createShapes():
-        shapes = []
-        num = 1
-        for size in ["little", "big"]:
-            for color in ["red", "blue"]:
-                for shape in ["circle", "rect"]:
-                    for filled in [True, False]:
-                        s = Shape(num, shape, color, size, filled, 100, 100)
-                        shapes.append(s)
-                        num += 1
-        return shapes
-
-
-    shapes = createShapes()
-    start = time.time()
-    tree = MinMaxTree(shapes=shapes)
-    tree.buildTree(True)
-    h = tree.getHRoot()
-    end = time.time()
-    total_time = end-start
-    print("h de root = ", h)
-    print("temps nécessaire (en s) : ", total_time)
-"""
